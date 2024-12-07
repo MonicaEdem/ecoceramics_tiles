bd9dfda643313d9570083973901dc13abb2b8972
Monica Edem Kokovena <monicaekokovena@gmail.com>
2024-12-07 12:08:48 +0000

    making changes to baseurl

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const API_KEY = 'AIzaSyBb1oSawvu-itpm6yTjf1EpW6kOjm4T7d4';
-const SPREADSHEET_ID = '1qbglRueU3Ztkfmb2MdgOtWatxHW-in0x1tvRGLKKGKg';
-
 export const fetchData = async (range) => {
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/1qbglRueU3Ztkfmb2MdgOtWatxHW-in0x1tvRGLKKGKg/values/sheet1!A1:D100?key=AIzaSyBb1oSawvu-itpm6yTjf1EpW6kOjm4T7d4`;
+    const url = import.meta.env.VITE_BASE_URL;
     try {
       const response = await axios.get(url);
       return response.data.values; // Return the data in a usable format
